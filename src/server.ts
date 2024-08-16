59a0112fad9ff388c64aa07afacdadf5c6777f9f
gablsl <gabrielsousadeveloper@gmail.com>
2024-08-16 16:28:42 -0300

    fix: wrong port

--- src/server.ts
+++ src/server.ts
@@ -52,8 +52,9 @@ fastify.delete<{ Params: TodoParams }>('/todos/:id', async (req, res) => {
 });
 
 const start = async () => {
+  const port = process.env.PORT ? parseInt(process.env.PORT, 10) : 3000;
   try {
-    await fastify.listen({ port: 3000, host: '0.0.0.0' });
+    await fastify.listen({ port: port, host: '0.0.0.0' });
   } catch (err) {
     fastify.log.error(err);
     process.exit(1);
